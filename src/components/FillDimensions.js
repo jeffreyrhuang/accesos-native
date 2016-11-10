29a81b8f693d0f911ec7832dec3cf87bfacd2d5b
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-11-10 09:21:40 -0600

    conditional diagram render based on state

--- src/components/FillDimensions.js
+++ src/components/FillDimensions.js
@@ -3,10 +3,20 @@ import { View, Text, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 class FillDimensions extends Component {
+
+  renderImage() {
+    const { orientation, segmentar } = this.props;
+    if (orientation === "Derecha" && segmentar === false) {
+      return(<Image source={require('../img/fill-der.png')} />)
+    } else if (orientation === "Izquierda" && segmentar === false) {
+      return(<Image source={require('../img/fill-izq.png')} />)
+    }
+  }
+
   render() {
     return(
       <View>
-        <Text>Dimensions Page</Text>
+        {this.renderImage()}
       </View>
     );
   }
