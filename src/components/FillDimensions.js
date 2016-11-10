3e70a02f340ec2bc350a7fb1bbbe7e2681966efc
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-11-10 15:17:19 -0600

    position a textinput box in one diagram

--- src/components/FillDimensions.js
+++ src/components/FillDimensions.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, TextInput, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 class FillDimensions extends Component {
 
   renderImage() {
     const { orientation, segmentar } = this.props;
+
     if (orientation === "Derecha" && segmentar === false) {
-      return(<Image source={require('../img/fill-der.png')} />)
+      return(
+        <Image source={require('../img/fill-der.png')}>
+          <View style={styles.derInput1}>
+            <TextInput style={styles.input} />
+          </View>
+        </Image>
+      )
     } else if (orientation === "Izquierda" && segmentar === false) {
       return(<Image source={require('../img/fill-izq.png')} />)
+    } else if (orientation === "Derecha" && segmentar) {
+      return(<Image source={require('../img/fill-seg-der.png')} />)
+    } else if (orientation === "Izquierda" && segmentar) {
+      return(<Image source={require('../img/fill-seg-izq.png')} />)
     }
   }
 
   render() {
     return(
-      <View>
+      <View style={styles.imageContainer}>
         {this.renderImage()}
       </View>
     );
+  }
+}
+
+const styles = {
+  imageStyle: {
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  derInput1: {
+    position: 'absolute',
+    width: 50,
+    height: 20,
+    left: 78,
+    bottom: 333
+  },
+  input: {
+    backgroundColor: '#eee',
+    flex: 1
   }
 }
 
