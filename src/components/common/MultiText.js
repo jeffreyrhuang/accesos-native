c556829e5ff6aa76a5c12811e9b56cbdee3b527e
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-12-15 18:41:33 -0600

    refactor MultiText into class component to show checkbox if filled

--- src/components/common/MultiText.js
+++ src/components/common/MultiText.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const MultiText = ({ label, onChangeText, placeholder, value }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.labelText}>
-        {label}
-      </Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        autoCorrect={false}
-        value={value}
-      />
-    </View>
-  );
-};
+class MultiText extends Component {
+
+  renderIcon() {
+    if (this.props.value) {
+      return <Icon name='check' size={18} style={{ alignSelf: 'flex-end', color: 'green' }} />;
+    }
+    return <Icon name='circle-thin' size={18} style={{ alignSelf: 'flex-end' }} />;
+  }
+
+  render() {
+    const { label, onChangeText, placeholder, value } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.topicContainer}>
+          <View style={styles.labelContainer}>
+            <Text style={styles.labelText}>
+              {label}
+            </Text>
+          </View>
+          <View style={styles.iconContainer}>
+            {this.renderIcon()}
+          </View>
+        </View>
+        <TextInput
+          multiline
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          autoCorrect={false}
+          value={value}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -27,8 +46,19 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderBottomWidth: 1
   },
+  topicContainer: {
+    flexDirection: 'row',
+    paddingRight: 10
+  },
+  iconContainer: {
+    flex: 1,
+    paddingTop: 2
+  },
   labelText: {
     fontSize: 16
+  },
+  labelContainer: {
+    flex: 8
   },
   input: {
     paddingLeft: 5,
